cf06dc4fe1a45c678bd4cca8b1583b803e4f9cd0
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-08-02 22:44:26 +0600

    nothing to say

---
+++ Js-string-object/revarce.js
@@ -0,0 +1,14 @@
+const sentence = "I'm learning web dev";
+
+let revers = "";
+for (latter of sentence) {
+  // console.log(latter);
+  revers = latter + revers;
+}
+
+for (let rever = 0; rever > sentence.length; rever++) {
+  // revers =
+  console.log(rever);
+}
+
+console.log(revers);
