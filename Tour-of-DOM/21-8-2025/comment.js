453e50446dc520b0ab9ae029bd9722a7686b5b83
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-08-21 15:35:02 +0600

    today learning addevenet

---
+++ Tour-of-DOM/21-8-2025/comment.js
@@ -0,0 +1,32 @@
+const commentSection = document.querySelector(".comment-section");
+const submit = document.getElementById("submit");
+let commentInput = document.getElementById("comment-input");
+
+//  actual function
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  // value validation
+
+  const textValue = commentInput.value.trim();
+
+  if (
+    commentInput.value.trim() === "" ||
+    commentInput.value.trim().length <= 10
+  ) {
+    alert(
+      "Please enter your valid comment anything and comment text length minimum 10  character"
+    );
+    return;
+  }
+  // const textValue = commentInput.value;
+
+  let createElementP = document.createElement("p");
+  createElementP.innerText = textValue;
+  createElementP.classList.add("comment");
+
+  //  appendChild
+  commentSection.appendChild(createElementP);
+
+  commentInput.value = "";
+});
