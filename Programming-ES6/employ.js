df383f247ced372ee82d346f09f16aba54e0f730
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-08-31 17:11:28 +0600

    learning js to es6

---
+++ Programming-ES6/employ.js
@@ -0,0 +1,21 @@
+const employ = {
+  name: "raja rani ",
+  designation: "QA",
+  salary: 20000,
+  experience: 1,
+};
+
+// const ent = Object.entries(employ);
+// console.log(ent);
+
+// const key = Object.keys(employ);
+// console.log(key);
+// const value = Object.values(employ);
+// console.log(value);
+
+// delete employ.experience;
+// console.log(employ);
+
+employ.salary = employ.salary + 5000;
+employ.location = "India";
+console.log(employ);
---
+++ Programming-ES6/object-lopping.js
@@ -0,0 +1,25 @@
+// const numbers = [12, 14, 14, 14, 25, 77, 45, 7, 57, 5, 557, 3, 578, 7, 787, 4];
+// for (const number of numbers) {
+//   console.log(number);
+// }
+
+// const employ = {
+//   name: "raja rani ",
+//   designation: "QA",
+//   salary: 20000,
+//   experience: 1,
+// };
+
+// const nayikas = [121, 15465, 335, 4344, 3, 54654];
+// const newA = [...nayikas];
+
+// (myFunction) => {};
+// const myFunction = () => {};
+
+const obj = {
+  foo: 1,
+};
+
+obj.bar = 5;
+
+console.log(obj);
