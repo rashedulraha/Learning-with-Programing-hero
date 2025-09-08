0264bca021622bdc72362a5381239bd2fe5564f5
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-09-08 16:40:53 +0600

    lerning js

---
+++ recap-basic-ES6/8-9-2025/practice-sesson/tamplate.js
@@ -0,0 +1,7 @@
+const name = "Hello world";
+
+const age = 20;
+const result = `your name is ${name} your age is ${
+  age > 18 ? age : "underage"
+}`;
+console.log(result);
---
+++ recap-basic-ES6/8-9-2025/practice-sesson/var-let-const.js
@@ -0,0 +1,17 @@
+// var name = "name";
+
+// let age = 12;
+// age = 56;
+// const roll = 23;
+
+// console.log(roll);
+// console.log(age);
+
+//  function scope
+function myFunc() {
+  var age = 2;
+  let name = 23;
+  const roll = 56;
+}
+
+console.log(age);
