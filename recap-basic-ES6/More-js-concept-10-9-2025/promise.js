eba5f56c5b7ebd48c96c853296e3a42331aba23f
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-09-10 08:31:12 +0600

    learning promise

--- recap-basic-ES6/More-js-concept-10-9-2025/promise.js
+++ recap-basic-ES6/More-js-concept-10-9-2025/promise.js
@@ -1,40 +1,58 @@
-const getData1 = new Promise((resolved, reject) => {
-  const number = Math.random() * 10;
-  console.log("generated number1 :", number);
+// const getData1 = new Promise((resolved, reject) => {
+//   const number = Math.random() * 10;
+//   console.log("generated number1 :", number);
 
-  if (number > 5) {
-    resolved({ num: number });
-  } else {
-    reject({
-      error: "data is not available",
-    });
-  }
-});
-const getData2 = new Promise((resolved, reject) => {
-  const number = Math.random() * 10;
-  console.log("generated2 number :", number);
+//   if (number > 5) {
+//     resolved({ num: number });
+//   } else {
+//     reject({
+//       error: "data is not available",
+//     });
+//   }
+// });
+// const getData2 = new Promise((resolved, reject) => {
+//   const number = Math.random() * 10;
+//   console.log("generated2 number :", number);
 
-  if (number > 5) {
-    resolved({ num: number });
-  } else {
-    reject({
-      error: "data is not available",
-    });
-  }
-});
-const getData3 = new Promise((resolved, reject) => {
-  const number = Math.random() * 10;
-  console.log("generated3 number :", number);
+//   if (number > 5) {
+//     resolved({ num: number });
+//   } else {
+//     reject({
+//       error: "data is not available",
+//     });
+//   }
+// });
+// const getData3 = new Promise((resolved, reject) => {
+//   const number = Math.random() * 10;
+//   console.log("generated3 number :", number);
 
-  if (number > 5) {
-    resolved({ num: number });
-  } else {
-    reject({
-      error: "data is not available",
-    });
-  }
-});
+//   if (number > 5) {
+//     resolved({ num: number });
+//   } else {
+//     reject({
+//       error: "data is not available",
+//     });
+//   }
+// });
 
-Promise.all([getData1, getData2, getData3])
-  .then((data) => console.log("Promise resolved : ", data))
-  .catch((error) => console.log(error));
+// Promise.all([getData1, getData2, getData3])
+//   .then((data) => console.log("Promise resolved : ", data))
+//   .catch((error) => console.log(error));
+
+// const promiseOne = new Promise((resolved, reject) => {
+//   const success = false;
+//   if (success) {
+//     resolved("Task done");
+//   } else reject("something want Wrong");
+// });
+
+// promiseOne
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((reject) => {
+//     console.error(reject);
+//   })
+//   .finally(() => {
+//     console.log("finished checked");
+//   });
