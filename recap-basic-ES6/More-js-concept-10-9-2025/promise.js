fe7433bc3c63cebeb4550162b6d8289015e5ea0f
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-09-10 08:20:05 +0600

    learning promise  toget data

--- recap-basic-ES6/More-js-concept-10-9-2025/promise.js
+++ recap-basic-ES6/More-js-concept-10-9-2025/promise.js
@@ -1,1 +1,40 @@
-const getData = new Promise();
+const getData1 = new Promise((resolved, reject) => {
+  const number = Math.random() * 10;
+  console.log("generated number1 :", number);
+
+  if (number > 5) {
+    resolved({ num: number });
+  } else {
+    reject({
+      error: "data is not available",
+    });
+  }
+});
+const getData2 = new Promise((resolved, reject) => {
+  const number = Math.random() * 10;
+  console.log("generated2 number :", number);
+
+  if (number > 5) {
+    resolved({ num: number });
+  } else {
+    reject({
+      error: "data is not available",
+    });
+  }
+});
+const getData3 = new Promise((resolved, reject) => {
+  const number = Math.random() * 10;
+  console.log("generated3 number :", number);
+
+  if (number > 5) {
+    resolved({ num: number });
+  } else {
+    reject({
+      error: "data is not available",
+    });
+  }
+});
+
+Promise.all([getData1, getData2, getData3])
+  .then((data) => console.log("Promise resolved : ", data))
+  .catch((error) => console.log(error));
