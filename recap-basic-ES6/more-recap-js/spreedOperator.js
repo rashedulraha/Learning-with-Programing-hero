d4cae5f4185aa0e28e5002132902492af2ffb079
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-09-12 16:07:25 +0600

    learning js destructureing and other functionality

---
+++ recap-basic-ES6/more-recap-js/spreedOperator.js
@@ -0,0 +1,41 @@
+//   using spreed operator
+
+// const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const arr5 = [0, ...arr1, 0, ...arr3];
+// console.log(arr5);
+
+//  copy object
+
+// const person = {
+//   name: "Rashedul islam",
+// };
+
+// const UpdatePerson = {
+//   ...person,
+//   age: 21,
+// };
+
+// console.log(UpdatePerson);
+// console.log(person);
+
+// const result = [2323, 3, 6, 5465, 464, 654, 4, 4, 465, 4];
+// const finalResult = Math.max(...result);
+// console.log(finalResult);
+
+//  object destructuring and use case
+// const personInfo = {
+//   name: "Rashedul islam",
+//   age: 45,
+//   job: "frontend developer",
+// };
+
+// //  destructuring normal way
+
+// const name1 = personInfo.name;
+// const age1 = personInfo.age;
+// const job1 = personInfo.job;
+
+// //  destructuring way to  object
+// const { name, age, job } = personInfo;
+// console.log(name, age, job);
