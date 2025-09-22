31c204f32e676d63c1f5a75f478771feaf5d4757
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-09-22 23:09:33 +0600

    learning js

--- Mailstone-7/vite-project/src/App.jsx
+++ Mailstone-7/vite-project/src/App.jsx
@@ -1,22 +1,7 @@
-import React, { Suspense } from "react";
-import Countries from "./components/countries/Countries";
-
 import "./App.css";
 
-// load api to promise
-
-const countriesLoadData = fetch(
-  `https://openapi.programming-hero.com/api/all`
-).then((res) => res.json());
-
 const App = () => {
-  return (
-    <div className="container">
-      <Suspense fallback={<p>Nadir vai loading...</p>}>
-        <Countries countriesLoadData={countriesLoadData} />
-      </Suspense>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- Mailstone-7/vite-project/src/components/Countrys/Count.jsx
+++
@@ -1,9 +0,0 @@
-// const Count = ({ visitedCountry }) => {
-//   return (
-//     <div>
-//       <h2> You visited Country : {visitedCountry} </h2>
-//     </div>
-//   );
-// };
-
-// export default Count;
