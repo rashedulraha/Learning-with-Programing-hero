55aa48f235e900a11806178cdac445196e1ee7d8
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-10-01 16:25:31 +0600

    learning react js daisy ui

--- Mailstone-7/sesson-class/src/Components/Navbar.jsx
+++ Mailstone-7/sesson-class/src/Components/Navbar.jsx
@@ -28,21 +28,13 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
             <li>
-              <a>Item 1</a>
+              <Link to={"/"}>Home</Link>
             </li>
             <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <Link to={"/about"}>About</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link to={"/contact"}>Contact</Link>
             </li>
           </ul>
         </div>
--- Mailstone-7/sesson-class/src/Pages/Home.jsx
+++ Mailstone-7/sesson-class/src/Pages/Home.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import PostCard from "../Components/PostCard";
+
 const Home = () => {
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://openapi.programming-hero.com/api/plants`)
+      .then((res) => res?.json())
+      .then((json) => setPlants(json.plants));
+  }, []);
+
   return (
     <>
-      <h1>This is our home section </h1>
+      <div className="container mx-auto  px-4 mt-5 mb-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 ">
+          {plants.map((plant) => (
+            <PostCard plant={plant} key={plant.id} />
+          ))}
+          {console.log(plants)}
+        </div>
+      </div>
     </>
   );
 };
