5218a36ab6e33bad21f25a0f714a165f580de7f7
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-10-27 16:56:13 +0600

    ...

--- Learning-node/script.js
+++ Learning-node/script.js
@@ -2,14 +2,33 @@ const express = require("express");
 
 const app = express();
 
+//  middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// create middleware
+
+app.use(function (req, res, next) {
+  console.log("middleware print");
+  next();
+});
+app.use(function (req, res, next) {
+  console.log("middleware print 2");
+  next();
+});
 // create a routes
 
 app.get("/", function (req, res) {
   res.send("champion mera anuj");
 });
 
-app.get("/profile", function (req, res) {
-  res.send("champion uska coach");
+app.get("/profile", function (req, res, next) {
+  return next(new Error("not implement"));
 });
 
-app.listen(5000);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+app.listen(3000);
