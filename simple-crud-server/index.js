d37b06bf1e2e08ea6f9af92c1754bb28d46b3c70
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-10-29 06:30:50 +0600

    learning simple server project

---
+++ simple-crud-server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ClientEncryption } = require("mongodb");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// ! middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://simpleDevUser:CWA2aQZp31vTJkLK@simpleproject.deo4wzy.mongodb.net/?appName=SimpleProject";
+
+//! Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/user", (req, res) => {
+  res.send("Hello user");
+});
+app.get("/profile", (req, res) => {
+  res.send("Hello profile");
+});
+
+app.listen(port, () => {
+  console.log(`prot is running : ${port}`);
+});
