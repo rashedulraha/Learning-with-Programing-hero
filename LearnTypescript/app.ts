e3bbae6f09431d16b955c82fe11c2228668db14d
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-10-29 14:21:08 +0600

    learn type inference

---
+++ LearnTypescript/app.ts
@@ -0,0 +1,29 @@
+// let arr: [number, string] = [2, "Nothing"];
+
+// enum UseRoles {
+//   ADMIN = 34,
+//   GUEST = "guest",
+//   SUPPER_ADMIN = "supper_admin",
+// }
+
+// enum StatusCodes {
+//   ABANDONED = "abandoned code 500",
+//   NOTFOUND = "not found status code 404",
+// }
+
+// const result = StatusCodes.ABANDONED;
+// console.log(result);
+
+// let a: number;
+// a = 4;
+
+// function Nothing(): void {
+//   console.log("hello world ");
+// }
+
+//!  type inference
+// let number: number | string | boolean;
+
+// number = 23;
+// number = "nothing";
+// number = true;
