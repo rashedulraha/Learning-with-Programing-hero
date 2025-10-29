0a375ddbe8a8d1e202bc2d705e378638d3031c7c
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-10-29 17:51:11 +0600

    create a data base and setup file

---
+++ simple-crus-client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Users from "./Components/Users";
+
+function App() {
+  return (
+    <>
+      <h1>Simple crud</h1>
+      <Users />
+    </>
+  );
+}
+
+export default App;
