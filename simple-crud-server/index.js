3080b57513d951384e409d1a405afbbb91ad102d
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-10-30 11:24:31 +0600

    setup server path

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -1,20 +1,19 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion, ClientEncryption } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PROT || 3000;
 
-// ! middleware
 app.use(cors());
 app.use(express.json());
 
-// const uri =
-//   "mongodb+srv://simpleDevUser:CWA2aQZp31vTJkLK@simpleproject.deo4wzy.mongodb.net/?appName=SimpleProject";
+//! get mongoBD uri
 const uri =
-  "mongodb+srv://simpleDevUser:CWA2aQZp31vTJkLK@simpleproject.deo4wzy.mongodb.net/simpleProjectDB?retryWrites=true&w=majority&appName=SimpleProject";
+  "mongodb+srv://simpleDevUser:CWA2aQZp31vTJkLK@simpleproject.deo4wzy.mongodb.net/?appName=SimpleProject";
 
-//! Create a MongoClient with a MongoClientOptions object to set the Stable API version
+//! mongoDB -> Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,24 +22,19 @@ const client = new MongoClient(uri, {
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
 async function run() {
   try {
     await client.connect();
-    const userDb = client.db("userDb");
-    const userCollection = userDb.collection("user");
 
-    //!  add data base related api
-    app.post("/user", async (req, res) => {
+    const userDB = client.db("userDB");
+    const userCollection = userDB.collection("/users");
+
+    app.post("/users", async function (res, req) {
       const newUser = req.body;
-      console.log("user information", newUser);
-
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
@@ -49,15 +43,17 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
-// app.get("/user", (req, res) => {
-//   res.send("Hello user");
-// });
-app.get("/profile", (req, res) => {
-  res.send("Hello profile");
+app.get("/", (req, res) => {
+  res.send("Hello i'm home");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Hello i'm user");
 });
 
 app.listen(port, () => {
-  console.log(`prot is running : ${port}`);
+  console.log(`server is running port number : ${port}`);
 });
